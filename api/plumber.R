a8b1c41fa2ea9ee3151df2289b88f2fa26c8e772
Tyler Richardett <tyler.richardett@gmail.com>
2020-03-14 18:34:24 -0400

    Players API build

---
+++ api/plumber.R
@@ -0,0 +1,15 @@
+#' @param id The unique identifier of the player(s).
+#' @serializer unboxedJSON
+#' @get /api/players
+function(id){
+    if (missing(id)) {
+        dbGetQuery(pool, "SELECT * FROM mls.players ORDER BY player_id")
+    } else {
+        split_ids <- strsplit(as.character(id), "\\s*,\\s*")
+        sql_ids <- paste0("('", paste0(split_ids[[1]], collapse = "', '"), "')")
+
+        sql <- "SELECT * FROM mls.players WHERE player_id IN ?id ORDER BY player_id"
+        query <- sqlInterpolate(pool, sql, id = SQL(sql_ids))
+        dbGetQuery(pool, query)
+    }
+}
