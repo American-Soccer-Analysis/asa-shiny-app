7f3a180bb74e03d219451b4e197e1937faecef7e
Tyler Richardett <tyler.richardett@gmail.com>
2020-06-26 08:51:16 -0400

    Fix date formatting in footer

--- app/utils/footer.R
+++ app/utils/footer.R
@@ -8,7 +8,7 @@ footer_reactive <- function(recent_games, client_timezone, database_timezone = D
     last_updated <- as.POSIXct(max(recent_games$last_updated_et, na.rm = TRUE), tz = database_timezone)
     attributes(last_updated)$tzone <- client_timezone
 
-    last_updated_date <- format(last_updated, "%B %M, %Y")
+    last_updated_date <- format(last_updated, "%B %d, %Y")
     last_update_hour <- as.numeric(format(last_updated, "%H"))
     last_updated_time <- gsub("^0", "", paste0(format(last_updated, "%I:%M"), ifelse(last_update_hour < 12, " a.m.", " p.m.")))
     last_update_tz <- format(last_updated, " %Z")
