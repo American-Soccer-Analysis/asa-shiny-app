d34e720effeefd9e6a612c15082aaca1c57cc2a4
Matthias Kullowatz <Matthias.Kullowatz@milliman.com>
2020-09-23 16:19:07 -0700

    Remove browser call

--- app/server.R
+++ app/server.R
@@ -5,7 +5,6 @@ server <- function(input, output, session) {
     # -----------------------------------------------
 
     # Source data -----------------------------------
-    browser()
     source("../app/utils/retrieve_data.R")
     source("../app/utils/reactive_values.R", local = TRUE)
 
