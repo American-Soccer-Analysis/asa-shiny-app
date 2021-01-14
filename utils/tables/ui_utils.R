cf61fe5019c6bd9d3599743b3f970f2a7113b007
Tyler Richardett <tyler.richardett@gmail.com>
2021-01-14 17:49:37 -0600

    ASA-95: Diacritics-neutralise

--- utils/tables/ui_utils.R
+++ utils/tables/ui_utils.R
@@ -100,7 +100,7 @@ tables_body <- function(page, league_config, client_timezone, tables_rv, filteri
         dt <- DT::datatable(
             df,
             extensions = c("Buttons", "FixedColumns", "Responsive"),
-            plugins = "accent-neutralise",
+            plugins = "diacritics-neutralise",
             callback = JS("setTimeout(function() { table.columns.adjust().fixedColumns().relayout(); }, 500);"),
             options = list(pageLength = 30,
                            autoWidth = FALSE,
